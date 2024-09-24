bd4ecaa18b0fca627388500eafbe2fbff3b3c17d
Ragot <Ragot@unicaen.fr>
2024-09-24 13:38:44 +0200

    Google font OK Esthétique mieux

--- script_pomodoro.js
+++ script_pomodoro.js
@@ -1,17 +1,18 @@
 let secondes = 0
 let minutes = 25
-let heures = 0
+
 let fini = false
 let parti = false
 
-let output = document.getElementById('lancer')
+let output = document.getElementById('affichage')
 let lancer_travail = document.getElementById('travail')
 let lancer_pause = document.getElementById('pause')
+
 lancer_travail.addEventListener('click' ,() => {
     parti = true
     secondes = 0
     minutes = 25
-    heures = 0
+
 })
 
 
@@ -19,7 +20,7 @@ lancer_pause.addEventListener('click', () =>{
     parti = true
     secondes = 0
     minutes = 5
-    heures = 0
+
 })
 
 function convert(){
@@ -38,28 +39,24 @@ function convert(){
         secondes = 59
         minutes--
     }
-    //Si les minutes arrivent à 0 et que le temps n'est pas fini (heure précedente)
+    //Si les minutes arrivent à 0 et que le temps n'est pas fini (Max 1H)
     if(minutes < 0 && !(fini)){
         minutes = 59
-        heures--
+        minutes = 0
     }
-    //Si les heures arrivent à 0 (on ne vas pas plus loin que les heures)
-    if(heures < 0 && !(fini)){
-        heures = 0
-    }
+
     //Si c'est fini, l'alert ne se déclenche qu'une fois, et les valeurs restent à 0
-    if(minutes == 0 && heures == 0 && secondes == 0){
-        heures = 0
+    if(minutes == 0  && secondes == 0){
         minutes = 0
         secondes = 0
         if(!(fini) || !(parti)){//Pas parti pour ne pas avoir l'alert au démarrage, pas fini pour que 
-                                //le temps se bloque a 0 et que les alertes n'apparaissent pas a l'infini
+                                +//le temps se bloque a 0 et que les alertes n'apparaissent pas a l'infini
             alert('temps écoulé')
         }
         fini = true
     }
 
-    output.textContent = (heures + ' : ' + minutes  + ' : ' + secondes)
+    output.textContent =("Restant : " + minutes + " Minutes " + ' : ' + secondes + " Secondes")
     if(!(fini)){
         secondes-- 
     }
